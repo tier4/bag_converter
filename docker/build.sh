ad30bbe7dd9cae53554954e664b71ba810c62af3
otenim <snake_istobelieve@keio.jp>
2025-11-11 20:15:40 +0900

    Add --no-cache option to build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,14 @@ cd ..
 # Build the Docker image
 echo "Building bag_converter Docker image..."
 
+# Check for --no-cache flag
+NO_CACHE_FLAG=""
+if [ "$1" = "--no-cache" ]; then
+    NO_CACHE_FLAG="--no-cache"
+    echo "Building without cache..."
+fi
+
 # Build the image from parent directory
-docker build -t bag_converter:latest -f docker/Dockerfile . --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --no-cache
+docker build $NO_CACHE_FLAG -t bag_converter:latest -f docker/Dockerfile . --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)
 
 echo "Docker image built successfully: bag_converter:latest"
